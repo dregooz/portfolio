8781bf2001baccd80f77d3019723e4aa0e751751
Andrea Nunez <dregoozdesign@gmail.com>
2018-09-19 15:45:35 -0700

    completed build process.

--- build.js
+++ build.js
@@ -3,51 +3,32 @@
 const Promise = require('bluebird');
 const sass = require('node-sass');
 const fs = require('fs');
-const fs = require('fse');
+const fse = require('fs-extra');
 
 _readDir('./views')
 .then(viewNames => {
   viewNames = viewNames.filter(viewName => viewName !== '.DS_Store');
 
-  console.log(viewNames);
-
   viewNames.forEach(viewName => buildView(viewName));
 })
+.then(() => console.log('Build completed successfully.'))
 .catch(err => console.log(err));
 
 
 
 function buildView(viewName) {
-  return Promise.props({
-    html: Promise.props({
-      index: _readFile(`./views/${viewName}/index.html`).then(buffer => buffer.toString())
-    }),
-    scss: _readDir(`./views/${viewName}`)
+  return Promise.join(
+    fse.copy(`./views/${viewName}/index.html`, `./dist/${viewName}/index.html`),
+    fse.copy(`./index.js`, `./dist/${viewName}/index.js`),
+    fse.copy(`./views/${viewName}/assets`, `./dist/${viewName}/assets`),
+    _readDir(`./views/${viewName}`)
     .then(files => {
-      const output = {};
-
       return Promise.map(files.filter(fileName => fileName.includes('.scss')), fileName => {
         return _sassToCss(`./views/${viewName}/${fileName}`)
-        .then(result => output[fileName] = result);
-      })
-      .then(() => output);
-    }),
-    js: _readFile(`./index.js`).then(buffer => buffer.toString()),
-    assets
-  })
-  .then(src => {
-    console.log(src);
-    return Promise.props({
-      html: Promise.props({
-        index: _writeFile(`./dist/${viewName}`, `index.html`, src.html.index)
-      }),
-      scss: Promise.map(Object.keys(src.scss), fileName => {
-        const css = src.scss[fileName];
-        _writeFile(`./dist/${viewName}`, fileName.replace(`scss`, `css`), css);
-      }),
-      js: _writeFile(`./dist/${viewName}`, 'index.js', src.js)
+        .then(css => _writeFile(`./dist/${viewName}`, fileName.replace(`scss`, `css`), css));
+      });
     })
-  });
+  );
 }
 
 
